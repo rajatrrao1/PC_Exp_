a4cee84f11bb1e081ff0e83a3abfda1b9c213fda
rajatrrao1 <67736211+rajatrrao1@users.noreply.github.com>
2023-11-08 14:07:08 +0100

    Update PressToContinue.m

--- PressToContinue.m
+++ PressToContinue.m
@@ -3,15 +3,23 @@ response     = 0;
 responsetime = NaN;
 
 if E.useDev.useBitsi
-    % TO DO
-    [response, responsetime] = E.bitsi.getResponse(ListenTime, true); % Wait for response within the remaining time
-    
-    E.bitsi.clearResponses();
-    
-    if ~isempty(resp)
-        % TO DO
-    end
-    
+    fprintf('Press a response key to continue...')
+    while 1
+        [resp, rt] = E.bitsi.getResponse(ListenTime, true); % Wait for response within the remaining time
+
+        if ~isempty(resp)
+            if resp == (E.keys.aKey) || resp == (E.keys.sKey) || resp == (E.keys.dKey)
+                response = resp; % Assign the response
+                responsetime = rt; % Assign the response time
+                break; % Exit the loop since a response was detected
+            end
+        end
+
+        % Check if the maximum listen time has been exceeded
+        if (GetSecs - starttime) > ListenTime
+            break; % Exit the loop if no response has been detected within the ListenTime
+        end
+    end    
 else
     fprintf('Press a response key to continue...')
     while 1
